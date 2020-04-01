2abc1b494e9167c2ea12ad46b4dafa127c397a44
Wai Phyo Thu <wopoto@gmail.com>
2020-04-01 10:02:48 +0630

    first commit

---
+++ src/screens/indexScreen.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import BlogContext from '../context/BlogContext';
+
+
+const IndexScreen = () => {
+    const value = useContext(BlogContext);
+
+    return <View><Text>Index Screen</Text></View>
+}
+
+
+const styles = StyleSheet.create({
+
+});
+
+
+export default IndexScreen;
