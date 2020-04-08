d03e1006b058d3012bb8334a0781a48eed5684ed
Wai Phyo Thu <wopoto@gmail.com>
2020-04-08 13:59:06 +0630

    update git config per project

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -5,9 +5,7 @@ import { EvilIcons } from '@expo/vector-icons';
 
 const ShowScreen = ({ navigation }) => {
     const { state } = useContext(Context);
-
     const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
-
 
     return <View>
         <Text>{blogPost.title}</Text>
