424b41efa553f3559954b03bc26d44c388c284ee
Wai Phyo Thu <waiphyothu@8bod.com>
2020-04-08 12:54:38 +0630

    Make common components Blog EditScreen & CreateScreen by using Context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,6 +2,13 @@ import createDateContext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case 'edit_blogpost':
+            return state.map(blogPost => {
+                return blogPost.id === action.payload.id
+                    ? action.payload
+                    : blogPost;
+
+            });
         case 'delete_blogpost':
             return state.filter((blogPost) => blogPost.id !== action.payload);
         case 'add_blogpost':
@@ -21,7 +28,9 @@ const blogReducer = (state, action) => {
 const addBlogPost = (dispatch) => {
     return (title, content, callback) => {
         dispatch({ type: 'add_blogpost', payload: { title, content } });
-        callback();
+        if (callback) {
+            callback();
+        }
     };
 };
 
@@ -31,11 +40,21 @@ const deleteBlogPost = (dispatch) => {
     };
 };
 
+const editBlogPost = dispatch => {
+    return (id, title, content, callback) => {
+        dispatch({ type: 'edit_blogpost', payload: { id, title, content } })
+        if (callback) {
+            callback();
+        }
+    };
+};
+
 export const { Context, Provider } = createDateContext(
     blogReducer,
     {
         addBlogPost,
-        deleteBlogPost
+        deleteBlogPost,
+        editBlogPost
     },
     []
 );
